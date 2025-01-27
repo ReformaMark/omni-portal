5ffe9ce7ab5e40383227e3394bde4f25775431bd
dmmcorp <ReformaMark>
2025-01-27 14:06:10 +0800

    fix: build errors

--- src/app/buyer/page.tsx
+++ src/app/buyer/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
